205751f08e9c53b71fe765757a2dd8b593e8a10e
Semwal-Gaurav07 <gauravsemwal362@gmail.com>
2026-08-14 17:01:55 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py makemigrations store accounts payment
+python manage.py migrate
+python seed_data.py
