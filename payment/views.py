d12f340c4a9e969661fb059f15006cd89aac8dfb
Semwal-Gaurav07 <gauravsemwal362@gmail.com>
2026-08-15 13:01:08 +0530

    Add interactive Card details form and UPI QR payment gateway

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.conf import settings
 from store.models import Order
 
 def payment_process(request):
@@ -7,22 +6,28 @@ def payment_process(request):
     order = get_object_or_404(Order, id=order_id)
 
     if request.method == 'POST':
-        payment_type = request.POST.get('payment_type')
+        payment_type = request.POST.get('payment_type', 'cod')
+        
         if payment_type == 'cod':
             order.paid = False
             order.payment_method = 'Cash on Delivery (COD)'
-            order.save()
+        elif payment_type == 'card':
+            card_num = request.POST.get('card_number', '4242')
+            last4 = card_num.replace(' ', '')[-4:] if len(card_num) >= 4 else '4242'
+            order.paid = True
+            order.payment_method = f'Credit/Debit Card (Ending in {last4})'
+        elif payment_type == 'upi':
+            upi_id = request.POST.get('upi_id', 'UPI')
+            order.paid = True
+            order.payment_method = f'UPI ({upi_id})'
         else:
             order.paid = True
             order.payment_method = 'Online Payment'
-            order.save()
+            
+        order.save()
         return redirect('payment:done')
 
-    return render(request, 'payment/process.html', {
-        'order': order,
-        'stripe_publishable_key': getattr(settings, 'STRIPE_PUBLISHABLE_KEY', ''),
-        'razorpay_key_id': getattr(settings, 'RAZORPAY_KEY_ID', '')
-    })
+    return render(request, 'payment/process.html', {'order': order})
 
 def payment_done(request):
     order_id = request.session.get('order_id')
